893479ab13df92ac2c17f34bf349b8409438d7fa
Liscare <liscare2@protonmail.com>
2021-04-10 19:23:19 +0200

    Delete print() into replace_links()

--- mdLinker.py
+++ mdLinker.py
@@ -9,7 +9,6 @@ def replace_links(file_name, links):
 
     with open(file_name, 'r+') as file:
         content = file.read()
-        print(content)
         for word, link in links.items():
             content = content.replace(word + " ", generate_link(word, link))
         file.write(content)
