878256295dd725cd6821aff5b4f2cfebd9f92792
Liscare <liscare2@protonmail.com>
2021-04-10 20:07:21 +0200

    Reformat method documentation

--- mdLinker.py
+++ mdLinker.py
@@ -2,9 +2,9 @@ def replace_links(file_name, links):
     """
     Replace all word in links by a markdown link according to its link
 
-        :param links: Dict of your words/links (example : {"word": "a link", "Framasoft":"https://framasoft.org/en/"})
-        :param file_name: File name of your markdown file
-        :return: None
+    :param links: Dict of your words/links (example : {"word": "a link", "Framasoft":"https://framasoft.org/en/"})
+    :param file_name: File name of your markdown file
+    :return: None
     """
 
     with open(file_name, 'r+') as file:
@@ -17,6 +17,7 @@ def replace_links(file_name, links):
 def replace_in_md(file_content, words_links):
     """
     Replace '[myWord]()' by '[myWord](wikiLink)' when myWord exists into words_links parameter
+
     :param file_content: Content of your markdown file
     :param words_links: Dict of each word and it link (example: {myWord: wikiLink})
     :return: File content with links
@@ -32,7 +33,7 @@ def generate_link(word, link):
     """
     Return a str like a markdown link
 
-        :return str (example: [python](https://en.wikipedia.org/wiki/Python_(programming_language)))
+    :return: str (example: [python](https://en.wikipedia.org/wiki/Python_(programming_language)))
     """
 
     return f'[{word}]({link})'
