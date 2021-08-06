2896c19431c4b5b143f2f2f65a031fc593f58ad1
Liscare <liscare2@protonmail.com>
2021-08-06 17:54:50 +0200

    From unittest to pytest library for Github Actions

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,35 +1,29 @@
-import unittest
 import cli
 import mock
 
 
-class MyTestCase(unittest.TestCase):
+class TestCli:
     def test_print_pretty_dict_simple(self):
         expected_result = "Wikipedia  Language Name\n" \
                           "Ejemplo 2  es        \n" \
                           "Example 1  en        \n" \
                           "Exemple 3  fr        \n"
-        self.assertEqual(expected_result,
-                         cli.print_pretty_dict({"Example 1": "en", "Ejemplo 2": "es", "Exemple 3": "fr"}))
+        assert expected_result == cli.print_pretty_dict({"Example 1": "en", "Ejemplo 2": "es", "Exemple 3": "fr"})
 
     def test_print_pretty_dict_complex(self):
         expected_result = "Wikipedia  Language Name\n" \
                           "de         German    \n" \
                           "es         Spanish   \n" \
                           "zh-classical Classical Chinese\n"
-        self.assertEqual(expected_result,
-                         cli.print_pretty_dict({"zh-classical": "Classical Chinese", "es": "Spanish", "de": "German"}))
+        assert expected_result == cli.print_pretty_dict(
+            {"zh-classical": "Classical Chinese", "es": "Spanish", "de": "German"})
 
     def test_choose(self):
         expected_result = "87"
         with mock.patch('builtins.input', side_effect=['1']):
-            self.assertEqual(expected_result, cli.choice(("54", "87", "12"), "A question ?"))
+            assert expected_result == cli.choice(("54", "87", "12"), "A question ?")
 
     def test_choose_with_3_errors(self):
         expected_result = "54"
         with mock.patch('builtins.input', side_effect=['d', '8', 'hello', '0']):
-            self.assertEqual(expected_result, cli.choice(("54", "87", "12"), "A question ?"))
-
-
-if __name__ == '__main__':
-    unittest.main()
+            assert expected_result == cli.choice(("54", "87", "12"), "A question ?")
