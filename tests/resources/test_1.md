67385b285b3e5fe434f3016038a775b03fc908e9
Liscare <liscare2@protonmail.com>
2021-08-08 23:26:37 +0200

    Create tests for wikiFetcher.py

---
+++ tests/resources/test_1.md
@@ -0,0 +1,5 @@
+[Danaus genutia](), the common tiger or striped tiger, is a species of brush-footed butterfly found in [Sri Lanka](), India, 
+Myanmar, [south-eastern]() Asia and [Australia](). It prefers areas of moderate to heavy rainfall, and typical habitats include 
+scrubby jungle, deciduous [forests]() and fallow land near habitations. The [insect] sequesters toxins from plants, and 
+advertises its unpalatability by having [prominent] () markings and striking colour patterns. This adult male common tiger,
+of the subspecies D. g. genutia, was photographed in [Kerala](), India]().
---
+++ tests/test_wikiFetcher.py
@@ -0,0 +1,15 @@
+import wikiFetcher
+import mock
+
+
+class TestWikiFetcher:
+    def test_fetch_from_md(self):
+        expected_result = ['Danaus genutia', 'Australia', 'forests', 'Sri Lanka', 'south-eastern', 'Kerala']
+        with open("./resources/test_1.md") as f:
+            file_tmp = f.read()
+        with mock.patch('builtins.input', return_value='0'):
+            result = wikiFetcher.fetch_from_md("./resources/test_1.md")
+            with open("./resources/test_1.md", 'w') as f:
+                f.write(file_tmp)
+            assert expected_result.sort() == result.sort()
+
