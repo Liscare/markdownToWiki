a97c5167fe1deef53cfa87f1e36849f2b494e5d8
Liscare <liscare2@protonmail.com>
2021-08-10 15:09:55 +0200

    Add tests folder in test paths

--- tests/test_wikiFetcher.py
+++ tests/test_wikiFetcher.py
@@ -5,11 +5,11 @@ import mock
 class TestWikiFetcher:
     def test_fetch_from_md(self):
         expected_result = ['Danaus genutia', 'Australia', 'forests', 'Sri Lanka', 'south-eastern', 'Kerala']
-        with open("./resources/test_1.md") as f:
+        with open("./tests/resources/test_1.md") as f:
             file_tmp = f.read()
         with mock.patch('builtins.input', return_value='0'):
-            result = wikiFetcher.fetch_from_md("./resources/test_1.md")
-            with open("./resources/test_1.md", 'w') as f:
+            result = wikiFetcher.fetch_from_md("./tests/resources/test_1.md")
+            with open("./tests/resources/test_1.md", 'w') as f:
                 f.write(file_tmp)
             assert expected_result.sort() == result.sort()
 
